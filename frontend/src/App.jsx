873f6ce9d491da61cb4966a85fde6deb7a24eff2
mesametamaarkhan <i221304@nu.edu.pk>
2025-02-10 21:46:55 +0500

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route, Router, useNavigate } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Footer from './components/Footer.jsx';
+import Navbar from "./components/Navbar.jsx";
+import Shop from './pages/Shop.jsx';
+import ProductDetail from './pages/ProductDetail.jsx';
+import SignupPage from './pages/SignUp.jsx';
+import LoginPage from './pages/Login.jsx';
+import CartPage from './pages/Cart.jsx';
+import Profile from './pages/Profile.jsx';
+import useTokenRefresher from './utility/UseTokenRefresher.js';
+import AboutUs from './pages/About.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const navigate = useNavigate();
+  useTokenRefresher(navigate);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path='/about' element={<AboutUs />} />
+        </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
